1da8b46c6a2870df98a1246176122405d8117a9d
kenan <kenabdulka@gmail.com>
2020-04-27 16:46:03 -0700

    added even listener and an anonymous function to display the correct dice image

--- game.js
+++ game.js
@@ -1,10 +1,85 @@
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 // scores for players 1 and 2
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+// To hide the dice we can:
+// document.querySelector(".dice").style.visibility = "hidden";
+// or
+// document.querySelector(".dice").style.display = "none";
+var diceImg = document.getElementsByClassName("dice")[0];
+diceImg.style.display = "none";
+
+var rollDiceBtn = document.getElementsByClassName("btn-roll")[0];
+
+// anonymous function to display the right dice number
+rollDiceBtn.addEventListener("click", function() {
+	var dice = Math.floor(Math.random() * 6) + 1;
+	diceImg.src = "dice-" + dice + ".png";
+	diceImg.style.display = "block";
+	document.querySelector("#current-" + activePlayer).textContent = dice;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
