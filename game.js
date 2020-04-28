881c305dfefe65c476c5c01e897da0cc8241e6a6
kenan <kenabdulka@gmail.com>
2020-04-27 20:48:18 -0700

    game can now determine active player and successfully calculates their scores and updates them

--- game.js
+++ game.js
@@ -15,10 +15,10 @@ var $currentPlayer0 = $("#current-0");
 $currentPlayer0.text("0");
 var $currentPlayer1 = $("#current-1");
 $currentPlayer1.text("0");
-var $score0 = $("#score-0");
-$score0.text("0");
-var $score1 = $("#score-1");
-$score1.text("0");
+var $totalScore0 = $("#score-0");
+$totalScore0.text("0");
+var $totalScore1 = $("#score-1");
+$totalScore1.text("0");
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
@@ -37,8 +37,56 @@ rollDiceBtn.addEventListener("click", function() {
 	var dice = Math.floor(Math.random() * 6) + 1;
 	diceImg.src = "dice-" + dice + ".png";
 	diceImg.style.display = "block";
-	document.querySelector("#current-" + activePlayer).textContent = dice;
+	// document.querySelector("#current-" + activePlayer).textContent = dice;
+
+	if (dice !== 1) {
+		roundScore += dice;
+		$("#current-" + activePlayer).text(roundScore);
+	} else {
+		scores[activePlayer] += roundScore;
+		$("#score-" + activePlayer).text(scores[activePlayer]);
+		roundScore = 0;
+		$("#current-" + activePlayer).text(roundScore);
+
+
+
+		if (activePlayer === 0) {
+			
+			activePlayer = 1;
+		} else {
+			
+			activePlayer = 0;
+		}
+		// or can use ternary operator
+		// activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	}
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
