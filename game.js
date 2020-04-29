85b31e3f4a10788baa27c5458dac6b636e5a1ee4
kenan <kenabdulka@gmail.com>
2020-04-28 17:21:06 -0700

    added state variable and initialize game function to implement the new game button

--- game.js
+++ game.js
@@ -1,24 +1,8 @@
 
 
+var scores, roundScore, activePlayer, gameWon;
 
-
-
-var scores, roundScore, activePlayer;
-
-// scores for players 1 and 2
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-
-var $currentPlayer0 = $("#current-0");
-$currentPlayer0.text("0");
-var $currentPlayer1 = $("#current-1");
-$currentPlayer1.text("0");
-var $totalScore0 = $("#score-0");
-$totalScore0.text("0");
-var $totalScore1 = $("#score-1");
-$totalScore1.text("0");
+initializeGame();
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
@@ -28,42 +12,53 @@ $totalScore1.text("0");
 // or
 // document.querySelector(".dice").style.display = "none";
 var diceImg = document.getElementsByClassName("dice")[0];
-diceImg.style.display = "none";
+
 
 var rollDiceBtn = document.getElementsByClassName("btn-roll")[0];
-
 // anonymous function to display the right dice number
 rollDiceBtn.addEventListener("click", function() {
-	var dice = Math.floor(Math.random() * 6) + 1;
-	diceImg.src = "dice-" + dice + ".png";
-	diceImg.style.display = "block";
-	// document.querySelector("#current-" + activePlayer).textContent = dice;
 
-	if (dice !== 1) {
-		roundScore += dice;
-		$("#current-" + activePlayer).text(roundScore);
-	} else {
-		// player has rolled a 1
-		// scores[activePlayer] += roundScore;
-		// $("#score-" + activePlayer).text(scores[activePlayer]);
-		roundScore = 0;
-		$("#current-" + activePlayer).text(roundScore);
-		switchPlayer();
+	if (!gameWon) {
+		var dice = Math.floor(Math.random() * 6) + 1;
+		diceImg.src = "dice-" + dice + ".png";
+		diceImg.style.display = "block";
+		// document.querySelector("#current-" + activePlayer).textContent = dice;
+
+		if (dice !== 1) {
+			roundScore += dice;
+			$("#current-" + activePlayer).text(roundScore);
+		} else {
+			// player has rolled a 1
+			// scores[activePlayer] += roundScore;
+			// $("#score-" + activePlayer).text(scores[activePlayer]);
+			roundScore = 0;
+			$("#current-" + activePlayer).text(roundScore);
+			switchPlayer();
+		}
 	}
-
 });
 
 
 // document.querySelector(".btn-hold").addEventListener("click", function() {
 // 	alert("clicked");
 // });
-$(".btn-hold").click(function() {
+$(".btn-hold").click(function(gameState) {
 	// alert("clicked");
-	scores[activePlayer] += roundScore;
-	$("#score-" + activePlayer).text(scores[activePlayer]);
-	
-	switchPlayer();
+	if (!gameWon) {
+		scores[activePlayer] += roundScore;
+		$("#score-" + activePlayer).text(scores[activePlayer]);
 
+		if (scores[activePlayer] >= 10) {
+			// player has won
+			$("#name-" + activePlayer).text("winner");
+			diceImg.style.display = "none";
+			$(".player-"+activePlayer+"-panel").addClass("winner");
+			$(".player-"+activePlayer+"-panel").toggleClass("active");
+			gameWon = true;
+		} else {
+			switchPlayer();	
+		}
+	}
 });
 
 // changes acive status and toggles active class
@@ -81,8 +76,37 @@ function switchPlayer() {
 	diceImg.style.display = "none";
 }
 
+$(".btn-new").click(initializeGame);
 
 
+function initializeGame() {
+	scores = [0,0];
+	roundScore = 0;
+	activePlayer = 0;
+	gameWon = false;
+
+	var $currentPlayer0 = $("#current-0");
+	$currentPlayer0.text("0");
+	var $currentPlayer1 = $("#current-1");
+	$currentPlayer1.text("0");
+	var $totalScore0 = $("#score-0");
+	$totalScore0.text("0");
+	var $totalScore1 = $("#score-1");
+	$totalScore1.text("0");
+	$("#name-0").text("Player 1");
+	$("#name-1").text("Player 2");
+	// document.querySelector(".player-0-panel").classList.remove("winner");
+	// $(".player-1-panel").removeClass("winner");
+	// document.getElementsByClassName("player-0-panel")[0].classList.remove("active");
+	// $("player-1-panel").removeClass("active");
+	$(".player-0-panel").removeClass("winner");
+	$(".player-1-panel").removeClass("winner");
+	$(".player-0-panel").removeClass("active");
+	$(".player-1-panel").removeClass("active");
+	$(".player-0-panel").addClass("active");
+	$(".dice").css("display", "none");
+
+}
 
 
 
